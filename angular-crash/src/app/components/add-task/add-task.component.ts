11c63204063dbb62882bfd58abb4b3c32392851a
Lukam90 <lucien.hamm@gmail.com>
2023-03-04 15:08:40 +0100

    MAJ Ex Create Task

--- angular-crash/src/app/components/add-task/add-task.component.ts
+++ angular-crash/src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { Task } from 'src/app/Task';
 
 @Component({
   selector: 'app-add-task',
@@ -6,7 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
   text: string = "";
   day: string = "";
   reminder: boolean = false;
+
+  onSubmit() {
+    if (! this.text) {
+      alert("Please add a task!");
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = "";
+    this.day = "";
+    this.reminder = false;
+  }
 }
